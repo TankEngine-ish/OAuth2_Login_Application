043353665bba22b6b610fba5ac88013f45c2dc23
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-02-04 22:47:03 +0200

    add https module

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const https = require('https');   
 const path = require('path');
 const express = require('express');
 
@@ -5,10 +6,17 @@ const PORT = 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/secret', (req, res) => {
+    res.send('Hello from server'); 
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+}).listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
