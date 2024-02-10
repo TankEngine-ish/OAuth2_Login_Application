69a57313b6e48cfd3c56a9ea62548554fb5c4200
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-02-10 02:42:00 +0200

    add passportjs and google oauth2 strategy

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(helmet()); // every request passes through helmet middleware
 
 
 function checkLoggedIn(req, res, next) {
-app.use((req, res, next) => {
     const isLoggedIn = true;
     if(!isLoggedIn) {
         return res.status(401).json({
@@ -25,14 +24,25 @@ app.use((req, res, next) => {
     next();
 }
 
+app.get('/auth/google', (req, res) => {});
+
+app.get('/auth/google/callback', (req, res) => {});
+
+app.get('/auth/logout', (req, res) => {});
 
 
 
 
 
 
-app.get('/secret', (req, res) => {
-    res.send('Hello from server'); 
+
+
+
+
+
+
+app.get('/secret', checkLoggedIn, (req, res) => {
+    return res.send('Hello from server'); 
 });
 
 app.get('/', (req, res) => {
