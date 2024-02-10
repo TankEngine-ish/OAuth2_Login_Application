3c4f217ecfefc4a2c98101daf0ee045ace001e15
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-02-10 18:06:27 +0200

    update dotenv secrets

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
 
+require('dotenv').config();
+
 const PORT = 3000;
+
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
 
 const app = express();
 
@@ -29,16 +36,6 @@ app.get('/auth/google', (req, res) => {});
 app.get('/auth/google/callback', (req, res) => {});
 
 app.get('/auth/logout', (req, res) => {});
-
-
-
-
-
-
-
-
-
-
 
 
 app.get('/secret', checkLoggedIn, (req, res) => {
